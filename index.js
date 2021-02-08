0b2de1c462b05923f4b7555851ad46910adeb840
michael <mpthornton86@gmail.com>
2021-02-08 15:44:51 -0700

    commit tree 0bfcc851bcd5a3ff040d8db4364da6e600e8595b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+const formName = document.getElementById('Name')
+const formEmail = document.getElementById('Email')
+
+let formData = {Name: `${formName.value}`, Email: `${formEmail.value}`}
+
+function submitData(name, email) {
+   return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+        name,
+        email
+        })
+   })
+   .then( function ( response ) {
+    return response.json()
+  } )
+  .then( function ( object ) {
+    document.body.innerHTML = object[ "id" ]
+  } )
+  .catch( function ( error ) {
+    document.body.innerHTML = error.message
+  } )
+}
